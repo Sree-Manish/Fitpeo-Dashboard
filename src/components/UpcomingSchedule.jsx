bc4d291407d74a96badf334fcb953781db6665cb
Sree Manish <csmanish2468@gmail.com>
2025-06-04 18:24:24 +0530

    Scan Overlays Updated

--- src/components/UpcomingSchedule.jsx
+++ src/components/UpcomingSchedule.jsx
@@ -7,21 +7,21 @@ const UpcomingSchedule = () => {
     <div className="Upschedule">
         <h3>The Upcoming Schedule</h3>
         <h4>On Thursday</h4>
-          <div className="appoint-cont">
-            {
-              ThursdayAppoints.map((item)=>{
-                return <SimpleAppointmentCard {...item}  />
-              })
-            }
-          </div>
+        <div className="appoint-cont">
+          {
+            ThursdayAppoints.map((item)=>{
+              return <SimpleAppointmentCard {...item}  />
+            })
+          }
+        </div>
         <h4>On Saturday</h4>
-          <div className="appoint-cont">
-            {
-              SaturdayAppoints.map((item)=>{
-                return <SimpleAppointmentCard {...item}  />
-              })
-            }
-          </div>
+        <div className="appoint-cont">
+          {
+            SaturdayAppoints.map((item)=>{
+              return <SimpleAppointmentCard {...item}  />
+            })
+          }
+        </div>
     </div>
   )
 }
